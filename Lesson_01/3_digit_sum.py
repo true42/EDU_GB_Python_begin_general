8b54a5bac1a94d687ed5f578a230b155bfb41679
BandS-Y <yuverch@gmail.com>
2021-01-19 18:02:55 +0700

    Lesson_01

---
+++ Lesson_01/3_digit_sum.py
@@ -0,0 +1,5 @@
+#  программа для баловства с цифрами
+
+digit = input('введите цифру: ')
+int(digit) + int(digit * 2) + int(digit * 3)
+print(f"сумма {digit} + {digit * 2} + {digit * 3} = {int(digit) + int(digit * 2) + int(digit * 3)}")
---
+++ Lesson_01/5_fin_mon.py
@@ -0,0 +1,15 @@
+#  Программа по финансовому мониторингу предприятия
+
+revenue = int(input("Введите сумму выручки: "))
+costs = int(input("Введите сумму издержек: "))
+# revenue = 456
+# costs = 123
+if revenue > costs:
+    profit = revenue - costs
+    print(f"Прибыль предприятия составлиа: {profit}")
+    print(f"Рентабельность выручки составила: {profit / revenue * 100:.02f}%")
+    employees = int(input("Введите количество штатных сотрудников: "))
+    print(f"Прибыль фирмы в рассчёте на одного сотрудника составляет: {profit / employees:.02f}")
+else:
+    print(f"К сожалению фирма сейчас находится в убытке {revenue - costs}")
+
